0def6474e885e4878206dab26991464fd769f0fe
yehuh <yeh_uh@yahoo.com.tw>
2020-03-01 07:48:35 +0000

    Transfer std_msgs/Int16

--- src/buzzer1.cpp
+++ src/buzzer1.cpp
@@ -1,17 +1,17 @@
 #include "ros/ros.h"
-#include "std_msgs/String.h"
+#include "std_msgs/Int16.h"
 #include <sstream>
 
-void callback(const std_msgs::String::ConstPtr& msg)
+void int16Callback(const std_msgs::Int16::ConstPtr& msg)
 {
- ROS_INFO("I heared: [%s]",msg->data.c_str());
+ ROS_INFO("I heared: %ld",(int)msg->data);
 }
 
 int main (int argc, char **argv)
 {
 ros::init(argc, argv,"buzzer");
 ros::NodeHandle n;
-ros::Subscriber sub = n.subscribe("chatter", 1000, callback);
+ros::Subscriber sub = n.subscribe("chatter", 1000, int16Callback);
 ros::spin();
 return 0;
 }
