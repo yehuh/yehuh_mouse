69489757fec5310b58e96cc87f2a455de33e9bc4
yehuh <yeh_uh@yahoo.com.tw>
2020-03-01 08:41:57 +0000

    control buzzer sucessfully

--- src/buzzer1.cpp
+++ src/buzzer1.cpp
@@ -1,10 +1,16 @@
 #include "ros/ros.h"
 #include "std_msgs/Int16.h"
 #include <sstream>
-
+#include <fstream>
+#include <iostream>
 void int16Callback(const std_msgs::Int16::ConstPtr& msg)
 {
  ROS_INFO("I heared: %ld",(int)msg->data);
+ int freq = msg->data;
+ std::ofstream buzz_file;
+ buzz_file.open("/dev/rtbuzzer0");
+ buzz_file << freq << std::endl;
+ buzz_file.close(); 
 }
 
 int main (int argc, char **argv)
