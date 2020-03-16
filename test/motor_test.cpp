48eb9cc23ce48e0b21893797d037d4a55e3b6918
yehuh <yeh_uh@yahoo.com.tw>
2020-03-16 07:49:39 +0000

    refine motor_test

--- test/motor_test.cpp
+++ test/motor_test.cpp
@@ -1,6 +1,41 @@
 #include "../include/yehuh_mouse/Motor.h"
 #include <gtest/gtest.h>
 
+TEST(MotorFixture, TestCmdVel)
+{
+ try
+ {
+  Motor m;
+  ros::Rate rate(0.3);
+  ros::Publisher pub = m.node_handle.advertise<geometry_msgs::Twist>("cmd_vel", QUEUE_SIZE);
+  int spin_count = 0;
+  while(ros::ok())
+  {
+   geometry_msgs::Twist twi_msg;
+   spin_count++;
+   if(spin_count < 2)
+   {
+    twi_msg.linear.x =   0.0000000;
+    twi_msg.angular.z =  1.57;
+    pub.publish(twi_msg);
+    ros::spinOnce();
+    rate.sleep();
+   }
+   else
+   {
+    twi_msg.linear.x =   0.0000;
+    twi_msg.angular.z =  0.0000;
+    pub.publish(twi_msg);
+   
+    ros::spinOnce();
+    rate.sleep();
+    if(spin_count> 2) break;
+   }
+  }
+ }
+ catch(...)
+ {FAIL()<<"CmdVel Apply Ero";}
+}
 
 TEST(MotorFixture, TestRawFreq)
 {
@@ -13,43 +48,6 @@ TEST(MotorFixture, TestRawFreq)
  }
  catch(...)
  {FAIL() << "Raw Freg Apply Ero";}
-}
-
-TEST(MotorFixture, TestCmdVel)
-{
- try
- {
-  Motor m;
-  ros::Rate rate(0.3);
-  ros::Publisher pub = m.node_handle.advertise<geometry_msgs::Twist>("cmd_vel", QUEUE_SIZE);
-  ros::Publisher pub_stop = m.node_handle.advertise<geometry_msgs::Twist>("cmd_vel", QUEUE_SIZE);
-  int spin_count = 0;
-  while(ros::ok())
-  {
-   geometry_msgs::Twist twi_msg;
-   spin_count++;
-   if(spin_count > 2)
-   {
-    twi_msg.linear.x =   0.0;
-    twi_msg.angular.z =  0.0;
-    pub_stop.publish(twi_msg);
-    ros::spinOnce();
-    rate.sleep();
-    if(spin_count> 4) break;
-   }
-   else
-   {
-    twi_msg.linear.x =   0.0;
-    twi_msg.angular.z = 1.57;
-    pub.publish(twi_msg);
-   
-    ros::spinOnce();
-    rate.sleep();
-   }
-  }
- }
- catch(...)
- {FAIL()<<"CmdVel Apply Ero";}
 }
 
 int main(int argc, char **argv)
